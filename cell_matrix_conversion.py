806b56e378fe19938c5af0a000662d24e7a55ed6
asif iqbal Bhatti <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2019-12-19 21:28:28 +0100

    Update cell_matrix_conversion.py

--- cell_matrix_conversion.py
+++ cell_matrix_conversion.py
@@ -5,7 +5,7 @@
 #    CODE to convert Cell Matrix to Cell Parameters
 #    VERSION: This script runs with python3 or later
 #    FORMAT of input file POSCAR VASP5 format
-#    DATE: 08/12/2019
+#    DATE: 19/12/2019
 #    USAGE: python3 sys.argv[0] [POSCAR]
 #####---------------------------------------------------------
 #####---------------------------------------------------------
@@ -60,22 +60,21 @@ def poscar():
 			gamma = math.degrees(math.acos(np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))))
 			alpha = math.degrees(math.acos(np.dot(b,c) / (np.linalg.norm(b) * np.linalg.norm(c))))
 			beta  = math.degrees(math.acos(np.dot(a,c) / (np.linalg.norm(a) * np.linalg.norm(c))))
-			vol = np.linalg.norm(a)*np.linalg.norm(b)*np.linalg.norm(c)*np.sqrt(1 + 2*math.cos(alpha)*math.cos(beta)*math.cos(gamma) - math.cos(alpha)**2 - math.cos(beta)**2 - math.cos(gamma)**2)
-			print ("ratio c/a = %2f" %(np.linalg.norm(c) / np.linalg.norm(a) ))
+			print ('\u03B1=', alpha, '\u03B2=', beta, '\u03B3=', gamma)
 			print ("#####------------------------------------------------")
-			print ('||a||=%2f, \u03B1= %2f' %(np.linalg.norm(a), alpha))
-			print ('||b||=%2f  \u03B2= %2f' %(np.linalg.norm(b), beta))
-			print ('||c||=%2f  \u03B3= %2f' %(np.linalg.norm(c), gamma))
-			print ('Vol= %2f' %(vol))			
+			print ('||a||=', np.linalg.norm(a))
+			print ('||b||=', np.linalg.norm(b))
+			print ('||c||=', np.linalg.norm(c)) 
 			break
 		else:
-			print ('NO file entered or wrong filename') 
+			print ('NO file eneterd or wrong file') 
 			break			
 #####---------------------------------------------------------
 # Looping over all directories in a current folder containing POSCARS files
 #####--------------------------------------------------------- 
 
 def main():
+	count = 0
 	os.system("rm out.dat")
 	mypath = os.getcwd()
 	#print (mypath)
@@ -85,78 +84,81 @@ def main():
 			#print (entry)
 			for file in os.listdir(entry):
 				#print (file)
-				filepath = os.path.join(entry, file)
-				#f = open(filepath, 'r')
-				#print (f.read())
-				#f.close()	
-				fo = open(filepath, 'r')
-				
-				ofile=open('out.dat','a+')
-				print (colored('>>>>>>>>  Name of the file: ','yellow'), fo.name, end = '\n', flush=True)
-				ofile.write (fo.name + '\n')
-				ofile.write ("")
-				firstline   = fo.readline()
-				secondfline = fo.readline()
-				Latvec1 = fo.readline()
-				#print ("Lattice vector 1:", (Latvec1), end = '')
-				#ofile.write (Latvec1)
-				Latvec2 = fo.readline()
-				#print ("Lattice vector 2:", (Latvec2), end = '')
-				#ofile.write (Latvec2)
-				Latvec3 = fo.readline()
-				#print ("Lattice vector 3:", (Latvec3), end = '')
-				#ofile.write (Latvec3)
-				elementtype=fo.readline()
-				#print ("Types of elements:", str(elementtype), end = '')
-				#ofile.write (str(elementtype))
-				numberofatoms=fo.readline()
-				#print ("Number of atoms:", (numberofatoms), end = '')
-				#ofile.write ((numberofatoms))
-				ofile.write ("\n")
-				
-				fo.close()
-	#####---------------------------------------------------------
-				a=[]; b=[]; c=[];
-				Latvec1=Latvec1.split()
-				Latvec2=Latvec2.split()
-				Latvec3=Latvec3.split()
-	#####---------------------------------------------------------
-				for ai in Latvec1:
-					a.append(float(ai))
-				for bi in Latvec2:
-					b.append(float(bi))
-				for ci in Latvec3:
-					c.append(float(ci))	
-				print ("#####------------------------------------------------")
-				print ('a=', a)
-				ofile.write ("'a=' {}\n".format(a))
-				print ('b=', b)
-				ofile.write ("'b=' {}\n".format(b))
-				print ('c=', c)
-				ofile.write ("'c=' {}\n".format(c))			
-	#####---------------------------------------------------------
-	### gamma = Cos-1( (a.b)/||a||.||b|| )
-	### alpha = Cos-1( (b.c)/||b||.||c|| )
-	### beta  = Cos-1( (a.c)/||a||.||c|| )
-	
-				gamma = math.degrees(math.acos(np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))))
-				alpha = math.degrees(math.acos(np.dot(b,c) / (np.linalg.norm(b) * np.linalg.norm(c))))
-				beta  = math.degrees(math.acos(np.dot(a,c) / (np.linalg.norm(a) * np.linalg.norm(c))))
-				print ('\u03B1=', alpha, '\u03B2=', beta, '\u03B3=', gamma)
-				ofile.write ("'\u03B1=' {} '\u03B2=' {} '\u03B3=' {}\n".format(alpha,beta,gamma))
-				print ("#####------------------------------------------------")
-				print ('||a||=', np.linalg.norm(a))
-				ofile.write ("'||a||=' {}\n".format(np.linalg.norm(a)))
-				print ('||b||=', np.linalg.norm(b))
-				ofile.write ("'||b||=' {}\n".format(np.linalg.norm(b)))			
-				print ('||c||=', np.linalg.norm(c)) 
-				ofile.write ("'||c||=' {}\n".format(np.linalg.norm(c)))			
-				ofile.write ("***************************************************\n")
-				ofile.close()
-
+				if file == "POSCAR":
+					count+=1
+					filepath = os.path.join(entry, file)
+					#f = open(filepath, 'r')
+					#print (f.read())
+					#f.close()	
+					fo = open(filepath, 'r')
+					ofile=open('out.dat','a+')
+					print (colored('>>>>>>>>  Name of the file: ','red'), fo.name, end = '\n', flush=True)
+					ofile.write (fo.name + '\n')
+					ofile.write ("")
+					firstline   = fo.readline()
+					secondfline = fo.readline()
+					Latvec1 = fo.readline()
+					#print ("Lattice vector 1:", (Latvec1), end = '')
+					#ofile.write (Latvec1)
+					Latvec2 = fo.readline()
+					#print ("Lattice vector 2:", (Latvec2), end = '')
+					#ofile.write (Latvec2)
+					Latvec3 = fo.readline()
+					#print ("Lattice vector 3:", (Latvec3), end = '')
+					#ofile.write (Latvec3)
+					elementtype=fo.readline()
+					#print ("Types of elements:", str(elementtype), end = '')
+					#ofile.write (str(elementtype))
+					numberofatoms=fo.readline()
+					#print ("Number of atoms:", (numberofatoms), end = '')
+					#ofile.write ((numberofatoms))
+					ofile.write ("\n")
+					
+					fo.close()
+		#####---------------------------------------------------------
+					a=[]; b=[]; c=[];
+					Latvec1=Latvec1.split()
+					Latvec2=Latvec2.split()
+					Latvec3=Latvec3.split()
+		#####---------------------------------------------------------
+					for ai in Latvec1:
+						a.append(float(ai))
+					for bi in Latvec2:
+						b.append(float(bi))
+					for ci in Latvec3:
+						c.append(float(ci))	
+					print ("#####------------------------------------------------")
+					print ('a=', a)
+					ofile.write ("'a=' {}\n".format(a))
+					print ('b=', b)
+					ofile.write ("'b=' {}\n".format(b))
+					print ('c=', c)
+					ofile.write ("'c=' {}\n".format(c))			
+		#####---------------------------------------------------------
+		### gamma = Cos-1( (a.b)/||a||.||b|| )
+		### alpha = Cos-1( (b.c)/||b||.||c|| )
+		### beta  = Cos-1( (a.c)/||a||.||c|| )
+		
+					gamma = math.degrees(math.acos(np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))))
+					alpha = math.degrees(math.acos(np.dot(b,c) / (np.linalg.norm(b) * np.linalg.norm(c))))
+					beta  = math.degrees(math.acos(np.dot(a,c) / (np.linalg.norm(a) * np.linalg.norm(c))))
+					print ('\u03B1=', alpha, '\u03B2=', beta, '\u03B3=', gamma)
+					ofile.write ("'\u03B1=' {} '\u03B2=' {} '\u03B3=' {}\n".format(alpha,beta,gamma))
+					print ("#####------------------------------------------------")
+					print ('||a||=', np.linalg.norm(a))
+					ofile.write ("'||a||=' {}\n".format(np.linalg.norm(a)))
+					print ('||b||=', np.linalg.norm(b))
+					ofile.write ("'||b||=' {}\n".format(np.linalg.norm(b)))			
+					print ('||c||=', np.linalg.norm(c)) 
+					ofile.write ("'||c||=' {}\n".format(np.linalg.norm(c)))			
+					ofile.write ("***************************************************\n")
+					ofile.close()
+	print ("Number of folders detected: ", count)
 if __name__ == "__main__":
 
-	poscar()
+	#poscar()
 
-	#main()
+	main()
+
+
 
