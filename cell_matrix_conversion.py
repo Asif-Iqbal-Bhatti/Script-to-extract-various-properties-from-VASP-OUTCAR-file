acce35fbabb45b8b574fd6f0bda4aff41c4784e4
asif iqbal Bhatti <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2019-12-22 20:11:26 +0100

    Update cell_matrix_conversion.py

--- cell_matrix_conversion.py
+++ cell_matrix_conversion.py
@@ -71,12 +71,13 @@ def poscar():
 			print ('NO file entered or wrong filename') 
 			break			
 #####---------------------------------------------------------
-# Looping over all directories in a current folder containing POSCAR files
+# Looping over all directories in a current folder containing POSCARS files
 #####--------------------------------------------------------- 
 
-def main():
+def main_poscar():
 	count = 0
 	os.system("rm out.dat")
+	VOL_P = [];
 	mypath = os.getcwd()
 	#print (mypath)
 	print ("               >>>>> Converting Cell Matrix to Cell Parameters <<<<<<")
@@ -128,7 +129,7 @@ def main():
 						b.append(float(bi))
 					for ci in Latvec3:
 						c.append(float(ci))	
-					print ("#####------------------------------------------------")
+					print ("////------------------------------------------------")
 					print ('a=', a)
 					ofile.write ("'a=' {}\n".format(a))
 					print ('b=', b)
@@ -136,14 +137,11 @@ def main():
 					print ('c=', c)
 					ofile.write ("'c=' {}\n".format(c))			
 		#####---------------------------------------------------------
-		### gamma = Cos-1( (a.b)/||a||.||b|| )
-		### alpha = Cos-1( (b.c)/||b||.||c|| )
-		### beta  = Cos-1( (a.c)/||a||.||c|| )
 		
-					gamma = math.degrees(math.acos(np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))))
-					alpha = math.degrees(math.acos(np.dot(b,c) / (np.linalg.norm(b) * np.linalg.norm(c))))
-					beta  = math.degrees(math.acos(np.dot(a,c) / (np.linalg.norm(a) * np.linalg.norm(c))))
-					VOLUME = np.dot(a, np.cross(b,c))		
+					alpha, beta, gamma = lattice_angles(a,b,c)
+					VOL_POS = np.dot(a, np.cross(b,c))	
+					VOL_P.append(VOL_POS)	
+					
 					print ('\u03B1=', alpha, '\u03B2=', beta, '\u03B3=', gamma)
 					ofile.write ("'\u03B1=' {} '\u03B2=' {} '\u03B3=' {}\n".format(alpha,beta,gamma))
 					print ("#####------------------------------------------------")
@@ -153,20 +151,143 @@ def main():
 					ofile.write ("'||b||=' {}\n".format(np.linalg.norm(b)))			
 					print ('||c||=', np.linalg.norm(c)) 
 					ofile.write ("'||c||=' {}\n".format(np.linalg.norm(c)))
-					print ('Vol= %3.5f' %(VOLUME))						
+					print ('Vol= %3.5f' %(VOL_POS))						
 					ofile.write ("***************************************************\n")
 					ofile.close()
 	print ("Number of folders detected: ", count)
+	return VOL_P
+	
+def main_contcar():
+	count = 0
+	os.system("rm out_contcar.dat")
+	VOL_C = [];	
+	mypath = os.getcwd()
+	print ("               >>>>> Converting Cell Matrix to Cell Parameters <<<<<<")
+	for entry in os.listdir(mypath):
+		if os.path.isdir(os.path.join(mypath, entry)):	
+			for file in os.listdir(entry):
+				if file == "CONTCAR":
+					count+=1
+					filepath = os.path.join(entry, file)
+					fo = open(filepath, 'r')
+					
+					ofile=open('out_contcar.dat','a+')
+					
+					print (colored('>>>>>>>>  Name of the file: ','yellow'), fo.name, end = '\n', flush=True)
+					ofile.write (fo.name + '\n')
+					ofile.write ("")
+					firstline   = fo.readline()
+					secondfline = fo.readline()
+					Latvec1 = fo.readline()
+					Latvec2 = fo.readline()
+					Latvec3 = fo.readline()
+					elementtype=fo.readline()
+					numberofatoms=fo.readline()
+					
+					ofile.write ("\n")
+					
+					fo.close()
+		#####---------------------------------------------------------
+					a=[]; b=[]; c=[];
+					Latvec1=Latvec1.split()
+					Latvec2=Latvec2.split()
+					Latvec3=Latvec3.split()
+		#####---------------------------------------------------------
+					for ai in Latvec1:
+						a.append(float(ai))
+					for bi in Latvec2:
+						b.append(float(bi))
+					for ci in Latvec3:
+						c.append(float(ci))	
+					print ("#####------------------------------------------------")
+					print ('a=', a)
+					ofile.write ("'a=' {}\n".format(a))
+					print ('b=', b)
+					ofile.write ("'b=' {}\n".format(b))
+					print ('c=', c)
+					ofile.write ("'c=' {}\n".format(c))			
+		#####---------------------------------------------------------
 
+					alpha, beta, gamma = lattice_angles(a,b,c)
+					VOL_CON = np.dot(a, np.cross(b,c))
+					VOL_C.append(VOL_CON)
+						
+					print ('\u03B1=', alpha, '\u03B2=', beta, '\u03B3=', gamma)
+					ofile.write ("'\u03B1=' {} '\u03B2=' {} '\u03B3=' {}\n".format(alpha,beta,gamma))
+					print ("#####------------------------------------------------")
+					print ('||a||=', np.linalg.norm(a))
+					ofile.write ("'||a||=' {}\n".format(np.linalg.norm(a)))
+					print ('||b||=', np.linalg.norm(b))
+					ofile.write ("'||b||=' {}\n".format(np.linalg.norm(b)))			
+					print ('||c||=', np.linalg.norm(c)) 
+					ofile.write ("'||c||=' {}\n".format(np.linalg.norm(c)))
+					print ('Vol= %4.6f ' %(VOL_CON)		)				
+					ofile.write ("***************************************************\n")
+					ofile.close()
+			#print (VOL_C)		
+	print ("Number of folders detected: ", count)
+	return VOL_C
+	
+	
+	
 #### math.sin fucntion takes arguments in radians ONLY
 def volume(a,b,c,alpha,beta,gamma):
 	length = np.linalg.norm(a) * np.linalg.norm(b) * np.linalg.norm(c) 
 	volume = length * ( np.sqrt(1 + 2 * math.cos(alpha) * math.cos(beta) * math.cos(gamma) - math.cos(alpha)**2 - math.cos(beta)**2 - math.cos(gamma)**2) )
 	return volume
 
+# Ordering of angles does matter
+def lattice_angles(a,b,c):
+	### gamma = Cos-1( (a.b)/||a||.||b|| )
+	### alpha = Cos-1( (b.c)/||b||.||c|| )
+	### beta  = Cos-1( (a.c)/||a||.||c|| )
+	gamma = math.degrees(math.acos(np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))))
+	alpha = math.degrees(math.acos(np.dot(b,c) / (np.linalg.norm(b) * np.linalg.norm(c))))
+	beta  = math.degrees(math.acos(np.dot(a,c) / (np.linalg.norm(a) * np.linalg.norm(c))))
+	return alpha, beta, gamma
+
+def volume_diff(VOL_P, VOL_C):
+	n=os.popen("find . -mindepth 1 -maxdepth 1 -type d | wc -l").read()
+	print ("VOLUME Diff %18s %12s %15.15s" %("CONTCAR",  "POSCAR",  "contcar-poscar"))
+	for i in range(int(n)):
+		print ("The difference is: %12.6f %12.6f %15.8f " %(VOL_C[i], VOL_P[i], VOL_C[i] - VOL_P[i]) )
+	
+	
 if __name__ == "__main__":
 
 	#poscar()
 
-	main()
+	VOL_P = main_poscar()
+	VOL_C = main_contcar()
+	print (" ----------------------------------------------------       ")
+	print (" ----------------------------------------------------       ")
+	print (" ----------------------------------------------------       ")
+	volume_diff(VOL_P, VOL_C)
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
