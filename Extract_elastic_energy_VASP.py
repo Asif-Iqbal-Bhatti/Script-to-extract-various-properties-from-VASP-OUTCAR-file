58fd138a284721eefbd0d159ac7793e78d786dd5
asif iqbal Bhatti <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2020-03-09 09:02:34 +0100

    Update Extract_elastic_energy_VASP.py

--- Extract_elastic_energy_VASP.py
+++ Extract_elastic_energy_VASP.py
@@ -163,47 +163,56 @@ class lattic_distortion():
 		print("HEA_atomic_size_mismatch: \u03B4={}".format(del_sum))
 	###
 		
-	def local_lattice_distortion_DEF2():
-		print ("Song, H. et al. Local lattice distortion in high-entropy alloys.")
-		print ("Phys. Rev. Mater. 1, 23404 (2017).")
-		print ("_____| Different definition of the atomic radius for the description ")
-		print ("       of the local lattice distortion in HEAs")
-		
-		if not os.path.exists('POSCAR' and 'CONTCAR'):
-			print (' ERROR: POSCAR & CONTCAR does not exist')
-			sys.exit(0)
-		print('Reading POSCAR and CONTCAR ... \n')
-		
-		x = []; y =[]; z=[]
-		xp =[]; yp = []; zp = []; temp=0
-		
-		f = open('POSCAR','r')
-		lines_poscar = f.readlines()
-		f.close()
-		
-		f = open('CONTCAR','r')
-		lines_contcar = f.readlines()
-		f.close()
-		
-		sum_atoms = lines_poscar[6].split()  ### reading 7th lines for reading # of atoms
-		sum_atoms = [int(i) for i in sum_atoms]
-		sum_atoms = sum(sum_atoms)
-		
-		for i in lines_poscar:
-			if "Direct" in i:
-				lp=lines_poscar.index(i)
-		for j in lines_contcar:
-			if "Direct" in j:
-				lc=lines_contcar.index(j)
-				
-		for i in range(sum_atoms):
-			x, y, z    = lines_poscar[lp+1+i].split()
-			xp, yp, zp = lines_contcar[lp+1+i].split()
-			x = float(x); y = float(y); z = float(z)
-			xp = float(xp); yp = float(yp); zp = float(zp)
-			temp = temp + np.sqrt( (x-xp)**2 + (y-yp)**2 + (z-zp)**2 )
-		temp = temp/sum_atoms
-		print("local lattice distortion: \u0394d={}".format(temp))	
+def local_lattice_distortion_DEF2():
+	print (">"*10,"local_lattice_distortion_DEF2")
+	print ("	Song, H. et al. Local lattice distortion in high-entropy alloys.")
+	print ("	Phys. Rev. Mater. 1, 23404 (2017).")
+	print ("	(***) Different definition of the atomic radius for the description ")
+	print ("	of the local lattice distortion in HEAs")
+	
+	if not os.path.exists('POSCAR' and 'CONTCAR'):
+		print ('>>> ERROR: POSCAR & CONTCAR does not exist (Both should be in the same directory)')
+		sys.exit(0)
+	print('Reading POSCAR and CONTCAR ... \n')
+	
+	x = []; y =[]; z=[]
+	xp =[]; yp = []; zp = []; temp=0
+	
+	f = open('POSCAR','r')
+	lines_poscar = f.readlines()
+	f.close()
+	
+	f = open('CONTCAR','r')
+	lines_contcar = f.readlines()
+	f.close()
+	
+	file_P = ase.io.read('POSCAR')
+	pos = file_P.get_cell_lengths_and_angles()
+	print (CRED + "POSCAR=>Length&Angles->{}".format(pos) + CEND)
+	file_C = ase.io.read('CONTCAR')
+	con = file_C.get_cell_lengths_and_angles() 
+	print (CRED + "CONTCAR=>Length&Angles->{}".format(con) + CEND)
+	print ("Cell vectors difference:: ",con-pos)
+	
+	sum_atoms = lines_poscar[6].split()  ### reading 7th lines for reading # of atoms
+	sum_atoms = [int(i) for i in sum_atoms]
+	sum_atoms = sum(sum_atoms)
+	
+	for i in lines_poscar:
+		if "Direct" in i:
+			lp=lines_poscar.index(i)
+	for j in lines_contcar:
+		if "Direct" in j:
+			lc=lines_contcar.index(j)
+			
+	for i in range(sum_atoms):
+		x, y, z    = lines_poscar[lp+1+i].split()
+		xc, yc, zc = lines_contcar[lp+1+i].split()
+		x = float(x); y = float(y); z = float(z)
+		xc = float(xc); yc = float(yc); zc = float(zc)
+		temp = temp + np.sqrt( (x-xc)**2 + (y-yc)**2 + (z-zc)**2 )
+	temp = temp/sum_atoms
+	print("local lattice distortion (LLD): \u0394d={}".format(temp))	
 
 ###
 def space_group_analyse(lattice, pos):
@@ -1011,90 +1020,104 @@ def mechanical_properties():
 	import numpy as np
 	import math, scipy, os, sys
 	from numpy import linalg as LA
-	np.set_printoptions(precision=6)
-
-	'''
-	# | Elastic Properties
-	# | Code for calculating mechanical properties 
-	#   from Energy vs Strain relationship
-	# 
-	# Equations can be found at Golesorkhtabar, R., Pavone, P., Spitaler, J., 
-	# Puschnig, P. & Draxl, C. ElaStic: A tool for calculating second-order elastic 
-	# constants from first principles. Comput. Phys. Commun. 184, 1861–1873 (2013).
-	'''
-
+	import statistics as st
+	import ase.io
+	
+	np.set_printoptions(precision=3)
+	CRED = '\033[91m';CEND = '\033[0m'
+	CYEL = '\033[33m'; CEND = '\033[0m'
+	CPIN = '\033[46m';
 	# Elastic constants have been obtained from SOEC approach mentioned in the above 
 	# paper. For three distortions of the crystal three constants are extracted: 
 	# C11, C44, C12. For cubic system the matrix is symmetric.
-	
-	print ("This script is for calculating mechanical properties")
-	print ("for cubic system. The values have to be supplied in the script.")
 
-	############################## INPUT PARAMETERS ################################
+	print (CRED + "Values has to be supplied in the script::" + CEND)
+
+	########################## INPUT PARAMETERS assuming cubic #######################
 	
-	c11=c22=c33=127.51 ; 
-	c44= 77.06/3 
-	B= 116.11
-	c12=c21=c13=c31=c23=c32=(1/6) * (9 * B - 3 * c11)
-	#c12=c21=c13=c31=c23=c32=80.86
-	
+	c11=c22=c33=123.22 ; 
+	c44=c55=c66=47
+	B = 120
+	#c12=c21=c13=c31=c23=c32=(1/6) * (9 * B - 3 * c11)
+	c12=c21=c13=c31=c23=c32=94.22
+	c14=c15=c16=c24=c25=c26=c34=c35=c36=0
+	c45=c46=c56=0
 	################################################################################
-	print ("-"*100)
-	print ("The STIFFNESS MATRIX Cij is:", end=("\n"))
-	Cij=[   [c11,c12,c13,0,0,0], 
-			[c21,c22,c23,0,0,0], 
-			[c31,c32,c33,0,0,0],
-			[0,0,0,c44,0,0],
-			[0,0,0,0,c44,0],
-			[0,0,0,0,0,c44]]
-	Cij=np.matrix(Cij)
+
+	print (CRED +"{:_^80s}".format("The STIFNESS MATRIX Cij is:")+ CEND)
+	Cij=[   [c11,c12,c13,c14,c15,c16], 
+			    [c21,c22,c23,c24,c25,c26], 
+			    [c31,c32,c33,c34,c35,c36],
+			    [0  ,0  ,0  ,c44,c45,c46],
+			    [0  ,0  ,0  ,0  ,c55,c56],
+			    [0  ,0  ,0  ,0  ,0  ,c66] ]
 	Cij=np.matrix(Cij).reshape((6,6))
-	
 	print (Cij)
+	
+	########------------Calculate variance and Mean of the Cij -----------
+	# ONLY for C11 , C22 , C33  
+	Cij_stat =  [ Cij[0,0] , Cij[1,1] , Cij[2,2] ]
+	print (">>>", (Cij_stat) )
+	STD = np.std(Cij_stat); Var = np.var(Cij_stat) ; Mean = np.mean(Cij_stat)
+	print ("{:6.3s} {:6.3s} {:6.3s}".format("C", "STD", "Var") )
+	print ("{:6.3f} {:6.3f} {:6.3f}".format(Mean, STD/np.sqrt(3), Var) )
+
+	######## ------------------------------------------------------------
+	
 	evals, eigenvec = LA.eig(Cij)
-	print ("-"*100)
-	print("The Eigenvalues are: ", evals)
-	#print("The Eigenvectors are: ")
-	#print(eigenvec)
-	print ("-"*100)
+	print ("-"*40)
+	print("Eigenvalues are: ", evals>0)
+	print ("-"*40)
 	# ### Compliance tensor  s_{ij}$ $(GPa^{-1})$
 	#  s_{ij} = C_{ij}^{-1}$
-	print ("-"*100)
-	print ("The COMPLIANCE MATRIX Sij is:", end=("\n"))
+	
+	print (CRED +"{:_^80s}".format("The COMPLIANCE MATRIX Sij is:")+ CEND)
 	Sij = np.linalg.inv(Cij)
-	print (Sij)
-	print ("-"*100)
+
+	print ("{} ".format(Sij))
+	print ("-"*80)
 	
-	# Voigt bulk modulus  K_v  $(GPa)$
-	# 9K_v = (C_{11}+C_{22}+C_{33}) + 2(C_{12} + C_{23} + C_{31}) 
-	Bv = ((Cij[0,0] + Cij[1,1] + Cij[2,2]) + 2 * (Cij[0,1] + Cij[1,2] + Cij[2,0])) / 9
+	######## -----------------------------VOIGT-------------------------------
+  
+	'''Voigt bulk modulus  (GPa)'''
 	
-	# Voigt shear modulus  G_v  $(GPa)$
-	#  15 G_v = (C_{11}+C_{22}+C_{33}) - (C_{12} + C_{23} + C_{31}) + 3(C_{44} + C_{55} + C_{66})$
-	Gv = ((Cij[0,0] + Cij[1,1] + Cij[2,2]) - (Cij[0,1] + Cij[1,2] + Cij[2,0]) + 3 * (Cij[3,3] + Cij[4,4] + Cij[5,5]))/15
+	#9K_v = (C_{11}+C_{22}+C_{33}) + 2(C_{12} + C_{23} + C_{31}) 
+	Bv = ((Cij[0,0] + Cij[1,1] + Cij[2,2]) + 2 * (Cij[0,1] + Cij[1,2] + Cij[2,0])) / 9.0
 	
-	## Poisson's ratio: Voigt
-	NuV = (3*Bv - Gv)/(6*Bv)
+	'''Voigt shear modulus  (GPa)'''
+	
+	#15*G_v = (C_{11}+C_{22}+C_{33}) - (C_{12} + C_{23} + C_{31}) + 3(C_{44} + C_{55} + C_{66})$
+	Gv = ((Cij[0,0] + Cij[1,1] + Cij[2,2]) - (Cij[0,1] + Cij[1,2] + Cij[2,0]) 
+	+ 3 * (Cij[3,3] + Cij[4,4] + Cij[5,5]))/15.0
 	
 	## Young's: Voigt
 	Ev = (9*Bv*Gv)/(3*Bv + Gv)
-	#--------------------------------------------------------------------------------------------
+		
+	## Poisson's ratio: Voigt
+	NuV = (3*Bv - Ev)/(6*Bv)
+	
+	######## -----------------------------REUSS-------------------------------
+	
 	# Reuss bulk modulus  K_R  $(GPa)$
 	#  1/K_R = (s_{11}+s_{22}+s_{33}) + 2(s_{12} + s_{23} + s_{31})$
-	Br = 1/((Sij[0,0] + Sij[1,1] + Sij[2,2]) + 2 * (Sij[0,1] + Sij[1,2] + Sij[2,0])) 
+	Br = 1/((Sij[0,0] + Sij[1,1] + Sij[2,2]) + 2*(Sij[0,1] + Sij[1,2] + Sij[2,0])) 
 	
 	# Reuss shear modulus  G_v  $(GPa)$
-	#   15/G_R = 4(s_{11}+s_{22}+s_{33}) - 4(s_{12} + s_{23} + s_{31}) + 3(s_{44} + s_{55} + s_{66})$
-	Gr = (1/15) * (4 * (Sij[0,0] + Sij[1,1] + Sij[2,2]) - (Sij[0,1] + Sij[1,2] + Sij[2,0]) + 3 * (Sij[3,3] + Sij[4,4] + Sij[5,5]))
-	
-	## Poisson's ratio: Reuss
-	NuR = (3*Br - Gr)/(6*Br)
+	# 15/G_R = 4*(s_{11}+s_{22}+s_{33}) - 4*(s_{12} + s_{23} + s_{31}) + 3(s_{44} + s_{55} + s_{66})$
+	Gr = (4 * (Sij[0,0] + Sij[1,1] + Sij[2,2]) - 4*(Sij[0,1] + Sij[1,2] + Sij[2,0]) 
+	+ 3 * (Sij[3,3] + Sij[4,4] + Sij[5,5]))
+	Gr = 15.0/Gr
 	
 	## Young's: Reuss
-	Er = (9*Br*Gr)/(3*Br + Gr)
-	##################################################################################
-	print ("{:30.8s} {:20.8s} {:20.8s} {:20.8s}".format(" ","Voigt", "Reuss ", "Hill") )
-	print ("-"*100)
+	Er = (9*Br*Gr)/(3*Br + Gr)	
+	
+	## Poisson's ratio: Reuss
+	NuR = (3*Br - Er)/(6*Br)
+
+	##########################################################################
+
+	######## -----------------------------Averages-------------------------------
+	
 	# #Hill bulk modulus  K_{VRH}$ $(GPa)$
 	#  K_{VRH} = (K_R + K_v)/2 
 	B_H = (Bv + Br)/2
@@ -1106,20 +1129,44 @@ def mechanical_properties():
 	#print ("VRH shear modulus (GPa): %20.8f " %(G_H) )
 	
 	# Young modulus E = 9BG/(3B+G)
-	E_H = 9 * B_H * G_H / (3 * B_H + G_H)
+	#E_H = (9 * B_H * G_H) / (3 * B_H + G_H)
+	E_H = (Ev + Er)/2
 	#print ("Young modulus E : {:1.8s} {:20.8f}".format(" ",E_H) )
 	
 	# ### Isotropic Poisson ratio $\mu 
 	# $\mu = (3K_{VRH} - 2G_{VRH})/(6K_{VRH} + 2G_{VRH})$
-	nu_H = (3 * B_H - 2 * G_H) / (6 * B_H + 2 * G_H )
+	#nu_H = (3 * B_H - 2 * G_H) / (6 * B_H + 2 * G_H )
+	nu_H = (NuV + NuR) / 2
 	#print ("Isotropic Poisson ratio: {:15.8f} ".format(nu_H) )
 	
-	print ("{:12.20s} {:20.6f} {:20.6f} {:20.6f}".format("Bulk Modulus (GPa)",Bv, Br, B_H) )
-	print ("{:12.20s} {:20.6f} {:20.6f} {:20.6f}".format("Shear Modulus(GPa)",Gv, Gr, G_H) )
-	print ("{:12.20s} {:20.6f} {:20.6f} {:20.6f}".format("Young Modulus(GPa)",Ev, Er, E_H) )
-	print ("{:18.20s} {:20.6f} {:20.6f} {:20.6f}".format("Poisson ratio ", NuV, NuR, nu_H) )
+	## Elastic Anisotropy
+	## Zener anisotropy for cubic crystals only
+	A = 2*(c44)/(c11-c12)
 	
-	print ("-"*100)
+	# Universal Elastic Anisotropy AU
+	AU = (Bv/Br) + 5*(Gv/Gr) - 6.0
+	
+	# C' tetragonal shear modulus
+	C = (c11-c12)/2
+	
+	ratio_V = Bv/Gv
+	ratio_R = Br/Gr
+	
+	print ("{:30.8s} {:20.8s} {:20.8s} {:20.8s}".format(" ","Voigt", "Reuss ", "Hill") )
+	print ("{:_^80}".format("GPa"))	
+	print ("{:16.20s} {:20.3f} {:20.3f} {:20.3f}".format("Bulk Modulus",Bv, Br, B_H) )
+	print ("{:16.20s} {:20.3f} {:20.3f} {:20.3f}".format("Shear Modulus",Gv, Gr, G_H) )
+	print ("{:16.20s} {:20.3f} {:20.3f} {:20.3f}".format("Young Modulus",Ev, Er, E_H) )
+	print ("{:16.20s} {:20.3f} {:20.3f} {:20.3f}".format("Poisson ratio ", NuV, NuR, nu_H) )
+	print ("{:16.20s} {:20.3f} {:20.3f} {:20.3f}({:5.3f})".format("B/G ratio ",Bv/Gv,Br/Gr, B_H/G_H, G_H/B_H) )
+	print ("{:16.20s} {:20.3s} {:20.3s} {:20.3f}".format("Avr ratio ",'','', (Gv-Gr)/(Gv+Gr)) )
+	print ("{:16.20s} {:20.3s} {:20.3s} {:20.3f}".format("Zener ratio ",'','', A) )
+	print ("{:16.20s} {:20.3s} {:20.3s} {:20.3f}".format("AU ",'','', AU) )
+	print ("{:16.20s} {:20.3s} {:20.3s} {:20.3f}".format("Cauchy pressure ",'','', (c12-c44)) )
+	print ("{:16.20s} {:20.3s} {:20.3s} {:20.3f}".format("C'tetra Shear ",'','',  C) )
+	
+	print ("-"*80)
+	return Sij
 ###
 
 
